5c6c1c9d0288fdc5cacc4785a7f986dc8409f895
Tatiana Barbova <44647094+Junumboxo@users.noreply.github.com>
2020-06-09 14:46:08 +0300

    Corrected variables names

--- sensors/DS18B20/DS18B20.cpp
+++ sensors/DS18B20/DS18B20.cpp
@@ -5,7 +5,7 @@
 OneWire      oneWire(DS18B20_PIN);  
 DallasTemperature sensor(&oneWire);
 
-float temp;
+float temp_DS18B20;
 
 void setupDS18B20(){  
   sensor.begin(); 
@@ -13,6 +13,6 @@ void setupDS18B20(){
 
 float getTemperatureDS18B20(void){
   sensor.requestTemperatures(); // Send the command to get temperature readings
-  temp = sensor.getTempCByIndex(0);
-  return temp;
+  temp_DS18B20 = sensor.getTempCByIndex(0);
+  return temp_DS18B20;
 }
